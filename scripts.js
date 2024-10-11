e0d1f6d529b57b3d8b51a914b00db896506bc794
Peter Bloem <git@peterbloem.nl>
2024-10-11 15:25:55 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -69,7 +69,7 @@ var lt = { // * top-level namespace
 	// Add new decks here
 	deck_files : [
 		['../decks/norwegian01.json', 0],
-		['../decks/nor-phrases.json', 1],
+		// ['../decks/nor-phrases.json', 1],
 		['../decks/norwegian02.json', 2],
 		['../decks/capitals.json', 3],
 		['../decks/us-states.json', 4],
